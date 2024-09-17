d5d87e72edf4f34413e423ecef13fed9700e2505
jolywelf15 <denisklykov3@gmail.com>
2024-09-18 01:00:10 +0300

    Add files via upload

---
+++ 56.pas
@@ -0,0 +1,18 @@
+﻿uses crt;
+var
+  x, y, first, second, third, difference: integer;
+begin
+  clrscr;
+  writeln('342:');
+  readln(x);
+  
+  first := x div 100;          
+  second := (x div 10) mod 10;
+  third := x mod 10;           
+  
+  y := third * 100 + second * 10 + first; 
+  difference := abs(x - y);               
+  
+  writeln('Разность между ', x, ' и ', y, ' равна ', difference);
+  readln;
+end.
