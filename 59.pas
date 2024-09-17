7b2948b023267dc93cac8a9dfc31f9fc6f571fa5
jolywelf15 <denisklykov3@gmail.com>
2024-09-18 01:19:12 +0300

    Add files via upload

---
+++ 59.pas
@@ -0,0 +1,13 @@
+﻿var a, b, c, p, S: real;
+begin
+writeln('10:');
+readln(a);
+writeln('10:');
+readln(b);
+writeln('10:');
+readln(c);
+p := (a + b + c) / 2;
+S := sqrt(p * (p - a) * (p - b) * (p - c));
+writeln('Площадь треугольника:', S);
+end.
+
