f87dd007adb9eb1cff0105e0557c0708e29f635f
jolywelf15 <denisklykov3@gmail.com>
2024-09-18 00:39:34 +0300

    Add files via upload

---
+++ 155.pas
@@ -0,0 +1,14 @@
+﻿program AddZero;
+var
+  num, result: string;
+begin
+  writeln('10:');
+  readln(num);
+  
+  if Length(num) > 0 then
+    result := '0' + num[Length(num)]
+  else
+    result := '0';
+  
+  writeln('Результат: ', result);
+end.
