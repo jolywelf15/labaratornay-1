edaceeae6b1d7493bc7bd7be36de90be0d16dc4a
jolywelf15 <denisklykov3@gmail.com>
2024-10-04 10:04:27 +0300

    Update main25.py

--- main25.py
+++ main25.py
@@ -1,6 +1,6 @@
 def find_entrance_and_floor(apartment_number):
   
-    apartments_per_entrance = 9 * 4  # 9 floors * 4 apartments per floor
+    apartments_per_entrance = 9 * 4 
     
     
     entrance_number = (apartment_number - 1) // apartments_per_entrance + 1
